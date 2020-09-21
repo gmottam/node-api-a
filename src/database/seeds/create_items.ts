7a9552b69b1ebdeec00971ed37fdb176c2f03f10
Guilherme Martins <guim.martins@outlook.com>
2020-09-21 18:27:05 -0300

    ajuste

--- src/database/seeds/create_items.ts
+++ src/database/seeds/create_items.ts
@@ -1,11 +1,19 @@
 import Knex from 'knex';
 export async function seed(knex: Knex) {
   await knex('items').insert([
-    { title: 'Wifi', image: 'wifi.svg' },
-    { title: 'Estacionamento', image: 'parking.svg' },
-    { title: 'Área Fumante', image: 'smoking.svg' },
-    { title: 'Massagem', image: 'massage.svg' },
-    { title: 'Café', image: 'coffee.svg' },
-    { title: 'Espaço Familia', image: 'family.svg' },
+    { title: 'Ambientes Climatizados', image: 'Ambientes_climatizados.svg' },
+    { title: 'Area Fumantes', image: 'Area_Fumantes.svg' },
+    { title: 'Aulas Especializadas', image: 'Aulas_Especializadas.svg' },
+    { title: 'Deficientes Fisicos', image: 'Deficientes_Fisicos.svg' },
+    { title: 'Esatcionamento', image: 'Esatcionamento.svg' },
+    { title: 'Guarda Volumes', image: 'Guarda_Volumes.svg' },
+    { title: 'Lanchonete', image: 'Lanchonete.svg' },
+    { title: 'Nutricionista', image: 'Nutricionista.svg' },
+    { title: 'Personal_Trainner', image: 'Personal_Trainner.svg' },
+    { title: 'Piscina', image: 'Piscina.svg' },
+    { title: 'Quadra', image: 'Quadra.svg' },
+    { title: 'Vestiarios', image: 'Vestiarios.svg' },
+    { title: 'Wifi', image: 'Wifi.svg' },
+    { title: 'Workshop', image: 'Workshop.svg' }
   ]);
 }
